8f52834471be2cd5a11f94bb51c2beb27e84e953
Karthiik26 <imkarthiik26@gmail.com>
2024-05-06 22:57:32 +0530

    update

--- DBconnection.js
+++ DBconnection.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://imkarthiik26:UCvCqcaaEs2iUhTs@tailordb.shtrlhw.mongodb.net/TailorApplication');
+mongoose.connect(process.env.Mongo_Url);
