b42e15c4d7dcfb8496be37f0b41db931501cba5a
Karthiik26 <imkarthiik26@gmail.com>
2024-05-08 12:44:04 +0530

    final4

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const http = require('http');
 const multer = require('multer');
 const cors = require('cors');
 const collection = require('./Schema')
-require('./DBconnection');
 const { v4: uuidv4 } = require('uuid');
 const AdminCollection = require('./AdminSchema');
 const TailorSchema = require('./TailorSchema');
@@ -14,6 +13,36 @@ const moment = require('moment')
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+require('./DBconnection');
+
+const mongoose = require('mongoose');
+// mongoose.connect(process.env.DB_CONNECTION)
+// mongoose.connect("mongodb+srv://imkarthiik26:UCvCqcaaEs2iUhTs@tailordb.shtrlhw.mongodb.net/TailorApplication")
+// mongoose.connect("mongodb://127.0.0.1:27017/TailorApplication")
+mongoose.connect("mongodb+srv://imkarthiik26:7bEX4PkTizP2LizG@tailorapp-db.q5nuy3b.mongodb.net/?retryWrites=true&w=majority&appName=TailorAPP-DB")
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.warn("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB:");
+});
+
+// Handle disconnects
+db.on("disconnected", () => {
+  console.warn("MongoDB disconnected");
+});
+
+// Close the MongoDB connection when the Node process is terminated
+process.on("SIGINT", () => {
+  db.close(() => {
+    console.log("MongoDB connection closed through app termination");
+    process.exit(0);
+  });
+});
 
 // port
 PORT = 4500;
