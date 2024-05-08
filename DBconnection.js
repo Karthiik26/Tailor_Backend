2cfc59449b3134be3245dd48a4aefc1a070186c2
Karthiik26 <imkarthiik26@gmail.com>
2024-05-08 12:31:49 +0530

    final2

--- DBconnection.js
+++ DBconnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.DB_CONNECTION)
 // mongoose.connect("mongodb+srv://imkarthiik26:UCvCqcaaEs2iUhTs@tailordb.shtrlhw.mongodb.net/TailorApplication")
 // mongoose.connect("mongodb://127.0.0.1:27017/TailorApplication")
-mongoose.connect("mongodb+srv://imkarthiik26:7bEX4PkTizP2LizG@tailorapp-db.q5nuy3b.mongodb.net/?retryWrites=true&w=majority&appName=TailorAPP-DB")
+mongoose.connect("mongodb+srv://imkarthiik26:7bEX4PkTizP2LizG@tailorapp-db.q5nuy3b.mongodb.net/?retryWrites=true&w=majority&appName=TailorAPP-DB/TailorApplication")
 
 const db = mongoose.connection;
 
